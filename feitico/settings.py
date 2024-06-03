08597feb6ca6a80af875471935fce53112392bd9
LucasBonii <158228644+LucasBonii@users.noreply.github.com>
2024-06-03 16:09:56 -0300

    Update settings.py

--- feitico/settings.py
+++ feitico/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'stockmanagement/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
